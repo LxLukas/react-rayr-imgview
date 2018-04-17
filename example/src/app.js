6e7bc82a7ea8588af42126c0beed6f804742d92d
luokaihui <luokaihui@didichuxing.com>
2018-04-17 13:28:30 +0800

    完善readme

--- example/src/app.js
+++ example/src/app.js
@@ -10,7 +10,10 @@ function App() {
     return (
         <div>
             <h1>组件初始化</h1>
-            <RayrImgview className="imgview-demo" alt='测试' src="http://img-hxy021.didistatic.com/static/woc/orion_mis/329fd5cf4202_20180326183623.jpg"/>
+            <RayrImgview className="imgview-demo" alt='测试' src="http://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1523951927125&di=122dea77b427ea2c1776137dba3012aa&imgtype=0&src=http%3A%2F%2Fpic35.photophoto.cn%2F20150529%2F0038038075710325_b.jpg"/>
+            <RayrImgview className="imgview-demo" alt='测试' src="http://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1523952852308&di=fee0106450330db13be4f5ab0328b27f&imgtype=0&src=http%3A%2F%2Fpic1.win4000.com%2Fwallpaper%2F6%2F58477701ec330.jpg"/>
+            <RayrImgview className="imgview-demo" alt='测试' src="http://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1523951927123&di=f5d1f68340a4b7551d9e1c2721f32572&imgtype=0&src=http%3A%2F%2Fpic42.nipic.com%2F20140605%2F9081536_142458626145_2.jpg"/>
+            
         </div>
     )
 }
