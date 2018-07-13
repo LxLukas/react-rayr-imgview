6a37b1c918194ffed2b23a5993cf260d194b8662
luokaihui <luokaihui@megvii.com>
2018-07-13 11:45:06 +0800

    feature:0.0.15版本：增加图片加载提示以及图片加载失败提示

--- example/src/app.js
+++ example/src/app.js
@@ -10,10 +10,9 @@ function App() {
     return (
         <div>
             <h1>组件初始化</h1>
-            <RayrImgview className="imgview-demo" alt='测试' src="http://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1523951927125&di=122dea77b427ea2c1776137dba3012aa&imgtype=0&src=http%3A%2F%2Fpic35.photophoto.cn%2F20150529%2F0038038075710325_b.jpg"/>
-            <RayrImgview className="imgview-demo" alt='测试' src="http://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1523952852308&di=fee0106450330db13be4f5ab0328b27f&imgtype=0&src=http%3A%2F%2Fpic1.win4000.com%2Fwallpaper%2F6%2F58477701ec330.jpg"/>
+            <RayrImgview className="imgview-demo" alt='测试' src="http://images.xlsdn.com/cHJvZF9pb3RfYzNzX3FJQ3R1K1JwMHdlQ2pSUE8xWTlSYkE9PQ==?auth_key=1531699200-0-0-549ddfebb7e8f875b4eeac74f88395cd"/>
+            <RayrImgview className="imgview-demo" alt='测试' src="src.jpg"/>
             <RayrImgview className="imgview-demo" alt='测试' src="http://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1523951927123&di=f5d1f68340a4b7551d9e1c2721f32572&imgtype=0&src=http%3A%2F%2Fpic42.nipic.com%2F20140605%2F9081536_142458626145_2.jpg"/>
-            
         </div>
     )
 }
